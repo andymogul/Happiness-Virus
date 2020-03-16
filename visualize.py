70738dc1cb729f763e6fe864c884e66c697cba7b
andymogul <andymogul@snu.ac.kr>
2020-03-16 23:30:55 +0900

    Add files via upload

--- visualize.py
+++ visualize.py
@@ -26,71 +26,83 @@ transform_test = transforms.Compose([
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
 
-raw_img = io.imread('images/1.jpg')
-gray = rgb2gray(raw_img)
-gray = resize(gray, (48,48), mode='symmetric').astype(np.uint8)
+def img2mood(raw_img):
 
-img = gray[:, :, np.newaxis]
+	cut_size = 44
 
-img = np.concatenate((img, img, img), axis=2)
-img = Image.fromarray(img)
-inputs = transform_test(img)
+	transform_test = transforms.Compose([
+	    transforms.TenCrop(cut_size),
+	    transforms.Lambda(lambda crops: torch.stack([transforms.ToTensor()(crop) for crop in crops])),
+	])
+	#raw_img = io.imread('images/1.jpg')
+	gray = rgb2gray(raw_img)
+	gray = resize(gray, (48,48), mode='symmetric').astype(np.uint8)
 
-class_names = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+	img = gray[:, :, np.newaxis]
 
-net = VGG('VGG19')
-checkpoint = torch.load(os.path.join('FER2013_VGG19', 'PrivateTest_model.t7'))
-net.load_state_dict(checkpoint['net'])
-net.cuda()
-net.eval()
+	img = np.concatenate((img, img, img), axis=2)
+	img = Image.fromarray(img)
+	inputs = transform_test(img)
 
-ncrops, c, h, w = np.shape(inputs)
+	class_names = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 
-inputs = inputs.view(-1, c, h, w)
-inputs = inputs.cuda()
-inputs = Variable(inputs, volatile=True)
-outputs = net(inputs)
+	net = VGG('VGG19')
+	checkpoint = torch.load(os.path.join('FER2013_VGG19', 'PrivateTest_model.t7'), map_location='cpu')
+	net.load_state_dict(checkpoint['net'])
+	#net.cuda()
+	net.eval()
 
-outputs_avg = outputs.view(ncrops, -1).mean(0)  # avg over crops
+	ncrops, c, h, w = np.shape(inputs)
 
-score = F.softmax(outputs_avg)
-_, predicted = torch.max(outputs_avg.data, 0)
+	inputs = inputs.view(-1, c, h, w)
+	#inputs = inputs.cuda()
+	inputs = Variable(inputs, volatile=True)
+	outputs = net(inputs)
 
-plt.rcParams['figure.figsize'] = (13.5,5.5)
-axes=plt.subplot(1, 3, 1)
-plt.imshow(raw_img)
-plt.xlabel('Input Image', fontsize=16)
-axes.set_xticks([])
-axes.set_yticks([])
-plt.tight_layout()
+	outputs_avg = outputs.view(ncrops, -1).mean(0)  # avg over crops
 
+	score = F.softmax(outputs_avg)
+	_, predicted = torch.max(outputs_avg.data, 0)
 
-plt.subplots_adjust(left=0.05, bottom=0.2, right=0.95, top=0.9, hspace=0.02, wspace=0.3)
+	emojis_img = io.imread('images/emojis/%s.png' % str(class_names[int(predicted.cpu().numpy())]))
 
-plt.subplot(1, 3, 2)
-ind = 0.1+0.6*np.arange(len(class_names))    # the x locations for the groups
-width = 0.4       # the width of the bars: can also be len(x) sequence
-color_list = ['red','orangered','darkorange','limegreen','darkgreen','royalblue','navy']
-for i in range(len(class_names)):
-    plt.bar(ind[i], score.data.cpu().numpy()[i], width, color=color_list[i])
-plt.title("Classification results ",fontsize=20)
-plt.xlabel(" Expression Category ",fontsize=16)
-plt.ylabel(" Classification Score ",fontsize=16)
-plt.xticks(ind, class_names, rotation=45, fontsize=14)
+	print("The Expression is %s" %str(class_names[int(predicted.cpu().numpy())]))
+	return int(predicted.cpu().numpy()), emojis_img
 
-axes=plt.subplot(1, 3, 3)
-emojis_img = io.imread('images/emojis/%s.png' % str(class_names[int(predicted.cpu().numpy())]))
-plt.imshow(emojis_img)
-plt.xlabel('Emoji Expression', fontsize=16)
-axes.set_xticks([])
-axes.set_yticks([])
-plt.tight_layout()
-# show emojis
-
-#plt.show()
-plt.savefig(os.path.join('images/results/l.png'))
-plt.close()
-
-print("The Expression is %s" %str(class_names[int(predicted.cpu().numpy())]))
+	'''
+	plt.rcParams['figure.figsize'] = (13.5,5.5)
+	axes=plt.subplot(1, 3, 1)
+	plt.imshow(raw_img)
+	plt.xlabel('Input Image', fontsize=16)
+	axes.set_xticks([])
+	axes.set_yticks([])
+	plt.tight_layout()
 
 
+	plt.subplots_adjust(left=0.05, bottom=0.2, right=0.95, top=0.9, hspace=0.02, wspace=0.3)
+
+	plt.subplot(1, 3, 2)
+	ind = 0.1+0.6*np.arange(len(class_names))    # the x locations for the groups
+	width = 0.4       # the width of the bars: can also be len(x) sequence
+	color_list = ['red','orangered','darkorange','limegreen','darkgreen','royalblue','navy']
+	for i in range(len(class_names)):
+	    plt.bar(ind[i], score.data.cpu().numpy()[i], width, color=color_list[i])
+	plt.title("Classification results ",fontsize=20)
+	plt.xlabel(" Expression Category ",fontsize=16)
+	plt.ylabel(" Classification Score ",fontsize=16)
+	plt.xticks(ind, class_names, rotation=45, fontsize=14)
+
+	axes=plt.subplot(1, 3, 3)
+	emojis_img = io.imread('images/emojis/%s.png' % str(class_names[int(predicted.cpu().numpy())]))
+	plt.imshow(emojis_img)
+	plt.xlabel('Emoji Expression', fontsize=16)
+	axes.set_xticks([])
+	axes.set_yticks([])
+	plt.tight_layout()
+	# show emojis
+
+	plt.show()
+	plt.savefig(os.path.join('images/results/l.png'))
+	plt.close()
+	'''
+	
